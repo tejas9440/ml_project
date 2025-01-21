0fd97f3819fd374454e06b8afaf9e0e38218687e
Tejas Parmar <153416264+tejas9440@users.noreply.github.com>
2025-01-21 14:05:12 +0000

    loadind model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+file_path = './popular.pkl'
+data = pd.read_pickle(file_path)
+
+# Display the data
+st.title('Popular Data Viewer')
+st.write(data)
+st.write("ended")
